a70bb448fba56a573999bbf618be4b6c360b5ea0
Mukseda Aktar <muksedamukti@gmail.com>
2025-08-08 20:49:36 +0600

    added task-1

---
+++ task-1.js
@@ -0,0 +1,7 @@
+function multiplyNumbers(num1, num2, num3, num4 ){
+    const result = num1*num2*num3*num4;
+    return result;
+}
+multiplyNumbers(5,2,3,1);
+const output = multiplyNumbers(5,2,3,1);
+console.log(output);
